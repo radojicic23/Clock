e16a74cf45603e62507b747b9f54c350eef22dfa
Aleksa <fankerman11@gmail.com>
2022-12-31 20:40:15 +0100

    Updated code structure.

--- Clock.py
+++ Clock.py
@@ -3,6 +3,7 @@ from tkinter.ttk import *
 
 from time import strftime
 
+
 # Create a UI (User Inteface) for the clock
 root = Tk()
 root.title('Clock')
@@ -15,14 +16,9 @@ def time():
     label.config(text=string)
     label.after(1000,time) # Call out time function for every second
     
-
 # Create a label to store our clock
 label = Label(root, font=('ds-digital', 60), background='black', foreground='green')
 label.pack(anchor='center')
 
 time()
 mainloop()
-
-
-
- 
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Digital Clock
 
 Simple Digital Clock in Python with Tkinter
+
+# Requirements
+- Python 3.x
+- Tkinter 
+
+# Run
+
+```bash
+git clone <repo-url>
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+```bash
+python Clock.py
+```
